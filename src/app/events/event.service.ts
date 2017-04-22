536c7cd9e595e6686ad7704bbb7d8ac29fe0a8c4
Paweł Gardziejewski <gardziejewski@gmail.com>
2017-04-22 11:24:27 +0200

    simple error handling

--- src/app/events/event.service.ts
+++ src/app/events/event.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 
 import { Event } from './interfaces/Event';
 
@@ -22,7 +23,7 @@ export class EventService {
         (response: Response) => {
           return response.json();
         }
-      );
+      ).catch(this.handleError);
   }
 
   getEvents() {
@@ -31,6 +32,20 @@ export class EventService {
 
   getEvent(index: number) {
     return this.events[index];
+  }
+
+  private handleError (error: Response | any) {
+    // use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
